dd03958e8fa693cfefb2105f18cbe395f6497dbb
Mhmd0Mhmod <iammhmdmhmod@gmail.com>
2024-09-24 12:57:21 +0300

    Global Styles With styled components

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,23 @@
 import styled from "styled-components";
 import GlobalyStyles from "./styles/GlobalStyled";
+import Button from "./ui/Button";
 
 const H1 = styled.h1`
   font-size: 2 rem;
-  color: red;
-`;
-const Button = styled.button`
-  font-size: 1.4rem;
-  padding: 1.2rem 1.6rem;
-  font-weight: 599;
-  border: none;
-  border-radius: 7px;
-  background-color: purple;
   color: white;
-  margin: 20px;
-  cursor: pointer;
 `;
-const Input = styled.input`
-  border: 1px solid #ddd;
-  border-radius: 5px;
-  padding: 0.8rem 1.2rem;
+const StyledApp = styled.main`
+  background-color: orangered;
+  padding: 20px;
 `;
 function App() {
   return (
     <>
       <GlobalyStyles />
-      <div>
-        <H1> Hello world</H1>
+      <StyledApp>
+        <H1>Styled Components</H1>
         <Button>Click me</Button>
-        <Input type="text" placeholder="Enter your name" />
-      </div>
+      </StyledApp>
     </>
   );
 }
--- src/ui/Button.jsx
+++ src/ui/Button.jsx
@@ -47,3 +47,20 @@ const variations = {
     }
   `,
 };
+const Button = styled.button`
+  font-size: 1.4rem;
+  padding: 1.2rem 1.6rem;
+  font-weight: 599;
+  border: none;
+  border-radius: var(--border-radius-sm);
+  background-color: var(--color-brand-500);
+  color: var(--color-brand-50);
+  box-shadow: var(--shadow-sm);
+  margin: 20px;
+  cursor: pointer;
+
+  &:hover {
+    background-color: var(--color-brand-600);
+  }
+`;
+export default Button;
